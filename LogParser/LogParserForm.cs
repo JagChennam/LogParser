b2a186feccfea0b7d87f03498d91803b98c29c58
Jag Chennam <jagadeesh.chennam@neudesic.com>
2020-11-23 16:26:05 -0700

    InitialChecking

--- LogParser/LogParserForm.cs
+++ LogParser/LogParserForm.cs
@@ -21,7 +21,7 @@ namespace LogParser
                 txtBox.Visible = true;
                 txtBox.Text = string.Empty;
                 lblSearchedFor.Visible = false;
-                string searchText = txtSearch.Text.Trim();
+                string searchText = txtSearch.Text.ToLower().Trim();
                 if (searchText == string.Empty)
                 {
                     searchText = "write protected memory";
@@ -32,33 +32,40 @@ namespace LogParser
 
                 DialogResult result = folderDlg.ShowDialog();
 
-                // look for all files with an extension of "log"
-
-                string[] files = System.IO.Directory.GetFiles(folderDlg.SelectedPath, "*.log");
-
-                if (files.Length == 0)
+                // check if user hit OK button on the dialog
+                if (result == DialogResult.OK)
                 {
-                    lblNoFile.Visible = true;
-                    txtBox.Visible = false;
-                    lblNoFile.Text = "No files with an extension of '.log' found in the selected directory..";
-                }
 
-                // if log files found, loop thru each file to find the search text
-                foreach (string file in files)
-                {
-                    lblSearchedFor.Visible = true;
-                    lblSearchedFor.Text = "You Searched for '" + searchText + "'";
-                    string text = File.ReadAllText(file).ToLower();
-                    int times = Regex.Matches(text, searchText).Count;
-                    txtBox.Font = new Font(FontFamily.GenericSerif, 14);
-                    int fileName = file.LastIndexOf("\\");
-                    txtBox.Text += "File Name: " + file.Substring(fileName).Replace("\\","") + System.Environment.NewLine + "Number of Occurences: " + times + System.Environment.NewLine;
-                    txtBox.Text += System.Environment.NewLine;
+                    // look for all files with an extension of "log"
+
+                    string[] files = System.IO.Directory.GetFiles(folderDlg.SelectedPath, "*.log");
+
+                    if (files.Length == 0)
+                    {
+                        lblNoFile.Visible = true;
+                        txtBox.Visible = false;
+                        lblNoFile.Text = "No files with an extension of '.log' found in the selected directory..";
+                    }
+
+                    // if log files found, loop thru each file to find the search text
+                    foreach (string file in files)
+                    {
+                        lblSearchedFor.Visible = true;
+                        lblSearchedFor.Text = "You Searched for '" + searchText + "'";
+                        string text = File.ReadAllText(file).ToLower();
+                        int times = Regex.Matches(text, searchText).Count;
+                        txtBox.Font = new Font(FontFamily.GenericSerif, 14);
+                        int fileName = file.LastIndexOf("\\");
+                        txtBox.Text += "File Name: " + file.Substring(fileName).Replace("\\", "") + System.Environment.NewLine + "Number of Occurrences: " + times + System.Environment.NewLine;
+                        txtBox.Text += System.Environment.NewLine;
+
+                    }
                 }
             }
 
             catch (Exception ex)
             {
+                lblNoFile.Visible = true;
                 lblNoFile.Text = ex.Message.ToString();
             }
         }
